997419684ebbb3f1ff62bb1786b48bb3b40d835f
IT19166834_K.S.L.K.Rathnasheela <it19166834@my.sliit.lk>
2023-12-07 12:05:11 +0530

    add changes for Tasting screen | desktop view

--- src/components/cocktails/tasting/Tasting.jsx
+++ src/components/cocktails/tasting/Tasting.jsx
@@ -9,13 +9,12 @@ function Tasting() {
                 className={['single-section']}
             >
                 <div className="first-section">
-                    <div id='first-section-title' className="first-section-title">
+                    <div className="first-section-left-container">
                         <h2>The original base spirit for punch</h2>
-                    </div>
-                    <div id='first-section-paragraph' className="first-section-paragraph">
                         <p>Since the 1600s, or even earlier, arrack has been used as the base spirit for numerous different types of punch. It is still widely used in punch cocktails today, thanks to its versatility and ability to both enhance and be enhanced by other natural flavours.</p>
                     </div>
-                    <div id='first-section-image' className="first-secion-image">
+
+                    <div className="first-section-right-container">
                         <img className='first-image' src='https://www.ceylonarrack.com/wp-content/uploads/2014/09/punch-bowl.png' alt='' />
                     </div>
                 </div>
@@ -60,7 +59,7 @@ function Tasting() {
                         <p>Originally drunk with coconut water by local Sri Lankan islanders, Ceylon Arrack is equally delicious when mixed with ginger beer or ginger ale, or blended as a base spirit in myriad cocktails. Our favourite way to serve it, however, is straight from the coconut shell.</p>
                     </div>
                     <div id='third-section-image' className="third-section-image">
-                        <img className='third-image' src='https://www.ceylonarrack.com/wp-content/uploads/2014/09/bottle.png' alt=''/>
+                        <img className='third-image' src='https://www.ceylonarrack.com/wp-content/uploads/2014/09/bottle.png' alt='' />
                     </div>
                 </div>
             </section>
