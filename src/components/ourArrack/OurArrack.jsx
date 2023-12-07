4cd7b42a947ab9dd4ce69bb660b5e35017756d82
hiruniMalshika <malshikahiruni99@gmail.com>
2023-12-07 23:05:49 +0530

    our arrack half page finished

--- src/components/ourArrack/OurArrack.jsx
+++ src/components/ourArrack/OurArrack.jsx
@@ -20,47 +20,90 @@ function OurArrack() {
   };
 
   return (
-    <div className='ourarrack-container'>
-      <div className='video-container'>
-        <ReactPlayer
-        className="react-player"
-        url="https://player.vimeo.com/video/103451402"
-        width="1400px"
-        height="700px"
-        playing={isPlaying}
-        volume= {isMuted ? 0 : 0.5}
-        controls={false}
-        onEnded={() => {
-          console.log("ended")
-        }}
-        config={{
-          file: {
-            attributes: {
-              playsInline: true,
-            },
-          },
-        }}
-        />
-      </div>
-      <div className='bottom-panel'>
-        <Button variant="secondary"
-        className={`control-play-button ${isPlaying ? "pause" : "play"}`}
-        onClick={handlePlayPause}
-        startIcon={isPlaying ? <PauseIcon /> : <PlayArrowIcon />}
-        >
-          {isPlaying ? "Pause" : "Play"}
-        </Button>
-        <Button variant="secondary"
-        className={`control-button ${isMuted ? "unmute" : "mute"}`}
-        onClick={handleMuteUnmute}
-        startIcon={isMuted ? <VolumeOffIcon /> : <VolumeUpIcon />}
-        >
-          {isMuted ? "Unmute": "Mute"}
-        </Button>
-      </div>
-    
+    <div className='outer-container'>
+      <section className='ourarrack-container'>
+          <ReactPlayer
+            className="video"
+            url="https://player.vimeo.com/video/103451402"
+            width="100%"
+            height="100%"
+            playing={isPlaying}
+            volume={isMuted ? 0 : 0.5}
+            controls={false}
+            onEnded={() =>{
+              console.log("ended")
+            }}
+            config={{
+              file: {
+                attributes: {
+                  playsInline: true,
+                }
+              }
+            }}
+            />
+      </section>
+      <section className='ourarrack-container'>
+        <div className='ourarrack-secondsection'>
+            <h2>Taken from trees</h2>
+        </div>
+      </section>
+      <section className='ourarrack-container'>
+        <div className='ourarrack-thirdsection'>
+          <div className='thirdsection-left'>
+              <p>Back in the early 1900s, in Sri Lanka, there was no such thing as high-quality arrack. Despite being an ancient, mystical spirit, first written about by Marco Polo in the 13th century, it was never considered to be finely made, or even particularly palatable. </p>
+          </div>
+          <div className='thirdsection-right'></div>  
+        </div>
+      </section>
+      <section className='ourarrack-container'>
+        4
+      </section>
+      <section className='ourarrack-container'>
+        5
+      </section>
     </div>
   )
 }
 
 export default OurArrack
+
+    // <div className='ourarrack-container'>
+    //   <div className='video-container'>
+    //     <ReactPlayer
+    //     className="react-player"
+    //     url="https://player.vimeo.com/video/103451402"
+    //     width="1400px"
+    //     height="700px"
+    //     playing={isPlaying}
+    //     volume= {isMuted ? 0 : 0.5}
+    //     controls={false}
+    //     onEnded={() => {
+    //       console.log("ended")
+    //     }}
+    //     config={{
+    //       file: {
+    //         attributes: {
+    //           playsInline: true,
+    //         },
+    //       },
+    //     }}
+    //     />
+    //   </div>
+    //   <div className='bottom-panel'>
+    //     <Button variant="secondary"
+    //     className={`control-play-button ${isPlaying ? "pause" : "play"}`}
+    //     onClick={handlePlayPause}
+    //     startIcon={isPlaying ? <PauseIcon /> : <PlayArrowIcon />}
+    //     >
+    //       {isPlaying ? "Pause" : "Play"}
+    //     </Button>
+    //     <Button variant="secondary"
+    //     className={`control-button ${isMuted ? "unmute" : "mute"}`}
+    //     onClick={handleMuteUnmute}
+    //     startIcon={isMuted ? <VolumeOffIcon /> : <VolumeUpIcon />}
+    //     >
+    //       {isMuted ? "Unmute": "Mute"}
+    //     </Button>
+    //   </div>
+    
+    // </div>
